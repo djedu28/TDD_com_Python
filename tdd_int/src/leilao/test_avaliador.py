20744b4051a6e2d31177c89da86cc8fd8402b353
Eduardo Santos <43933482+djedu28@users.noreply.github.com>
2021-09-08 04:46:45 -0300

    Segundo teste

--- tdd_int/src/leilao/test_avaliador.py
+++ tdd_int/src/leilao/test_avaliador.py
@@ -26,3 +26,25 @@ class TestAvaliador(TestCase):
 
         self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
         self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+
+def test_avalia(self):
+
+    users = [
+        Usuario('Edu'),
+        Usuario('Karol')
+    ]
+
+    leilao = Leilao('Celular')
+
+    leilao.lances.append(Lance(users[1], 150.0)) # LANCE da Karol
+    leilao.lances.append(Lance(users[0], 100.0)) # LANCE de Edu
+
+
+    avaliador = Avaliador()
+    avaliador.avalia(leilao)
+
+    menor_valor_esperado = 100.0
+    maior_valor_esperado = 150.0
+
+    self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
+    self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
