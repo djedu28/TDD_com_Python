5e6a0bb47e8d5e84ee8aed3a732d92a4b992d0a5
Eduardo Santos <43933482+djedu28@users.noreply.github.com>
2021-09-08 04:57:17 -0300

    renomeando testes

--- tdd_int/src/leilao/test_avaliador.py
+++ tdd_int/src/leilao/test_avaliador.py
@@ -5,7 +5,7 @@ from src.leilao.dominio import Usuario, Lance, Leilao, Avaliador
 
 class TestAvaliador(TestCase):
 
-    def test_avalia(self):
+    def test_quando_adicionados_em_ordem_crescente_deve_retornar_o_maior_e_o_menor_valor_de_um_lance(self):
         # LANCE EM ORDEM DECRESCENTE
 
         users = [
@@ -28,8 +28,8 @@ class TestAvaliador(TestCase):
         self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
         self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
 
-    def test_avalia2(self):
-        # LANCE EM ORDEM CRESSENTE
+    def test_quando_adicionados_em_ordem_decrescente_deve_retornar_o_maior_e_o_menor_valor_de_um_lance(self):
+        # LANCE EM ORDEM CRESCENTE
         users = [
             Usuario('Edu'),
             Usuario('Karol')
