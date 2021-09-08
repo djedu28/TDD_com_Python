677df13bf23a8af970e1af709854a42446c9b560
Eduardo Santos <43933482+djedu28@users.noreply.github.com>
2021-09-08 05:08:44 -0300

    add teste 3 LANCES, 2 USUARIOS

--- tdd_int/src/leilao/test_avaliador.py
+++ tdd_int/src/leilao/test_avaliador.py
@@ -53,8 +53,8 @@ class TestAvaliador(TestCase):
 
 
     def test_quando_tiver_um_lance_deve_retornar_o_mesmo_valor_no_maior_e_menor_lance(self):
-
         # LANCE UNICO
+
         lance = 100.0
 
         users = [
@@ -71,5 +71,35 @@ class TestAvaliador(TestCase):
 
         self.assertEqual(lance, avaliador.menor_lance)
         self.assertEqual(lance, avaliador.menor_lance)
+
+
+    def test_quando_o_leilao_tiver_tres_lances_deve_retornar_o_maior_e_o_menor_valor(self):
+        # 3 LANCES, 2 USUARIOS
+
+        users = [
+            Usuario('Edu'),
+            Usuario('Karol'),
+        ]
+        lances = [
+            100.0,
+            150.0,
+            128.0,
+        ]
+        leilao = Leilao('Celular')
+
+        leilao.lances.append(Lance(users[0], lances[0])) # LANCE de Edu
+        leilao.lances.append(Lance(users[1], lances[1])) # LANCE da Karol
+        leilao.lances.append(Lance(users[1], lances[2])) # LANCE da Carlos
+
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        menor_valor_esperado = min(lances)
+        maior_valor_esperado = max(lances)
+
+        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
+        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+
 
     # def test_quando_..._deve_...(self):
