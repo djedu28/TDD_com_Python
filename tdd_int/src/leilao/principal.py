1d65bdc04ce5b92269a58f2d12ab8f8bb0700687
Eduardo Santos <43933482+djedu28@users.noreply.github.com>
2021-09-08 04:32:47 -0300

    Printando valores com casa decimal usando virgula

--- tdd_int/src/leilao/principal.py
+++ tdd_int/src/leilao/principal.py
@@ -1,4 +1,7 @@
 from src.leilao.dominio import Usuario, Lance, Leilao, Avaliador
+def num2rs(valor):
+    """Separador de cadas decimais para o padrão BR"""
+    return str(valor).sub(".",",")
 
 users = [
     Usuario('Edu'),
@@ -13,10 +16,10 @@ leilao.lances.append(Lance(users[1], 150.0)) # LANCE da Karol
 
 print("\n")
 for lance in leilao.lances:
-    print(f'O usuario {lance.usuario.nome} deu um lance de R$ {lance.valor}')
+    print(f'O usuario {lance.usuario.nome} deu um lance de R$ {num2rs(lance.valor)}')
 
 avaliador = Avaliador()
 avaliador.avalia(leilao)
 
-print(f'O menor lance foi de R$ {avaliador.menor_lance} e o maior lance foi de R$ {avaliador.maior_lance}')
+print(f'O menor lance foi de R$ {num2rs(avaliador.menor_lance)} e o maior lance foi de R$ {num2rs(avaliador.maior_lance)}')
 print("\n")
